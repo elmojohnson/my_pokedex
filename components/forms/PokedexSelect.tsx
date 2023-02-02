ff55567af360f87c278f5868ea4031f458ecab7b
John Elmo Johnson <elmoj6@gmail.com>
2023-02-02 17:28:04 +1100

    Update: Pokedex select UI

--- components/forms/PokedexSelect.tsx
+++ components/forms/PokedexSelect.tsx
@@ -2,13 +2,20 @@ import React, { useContext } from "react";
 import PokedexContext from "@/contexts/PokedexContext";
 
 const PokedexSelect = () => {
-  const { pokedexes, currentPokedex, handleChange } = useContext(PokedexContext);
+  const { pokedexes, currentPokedex, handleChange } =
+    useContext(PokedexContext);
   return (
-    <select value={currentPokedex} onChange={handleChange}>
-      {pokedexes.map((pokedex) => {
-        return <option value={pokedex.url} key={pokedex.name}>{pokedex.name}</option>;
-      })}
-    </select>
+    <div className="bg-accent px-3 rounded-full overflow-hidden hover:brightness-95">
+      <select value={currentPokedex} onChange={handleChange} className="bg-transparent py-1 outline-none text-white font-semibold capitalize">
+        {pokedexes.map((pokedex) => {
+          return (
+            <option value={pokedex.url} key={pokedex.name} className="text-black capitalize">
+              {pokedex.name}
+            </option>
+          );
+        })}
+      </select>
+    </div>
   );
 };
 
